666f0af3db57d437efda789c19e41a41a49f87ba
Mendy Ben-Ezra <mbembe770@gmail.com>
2022-08-07 14:08:03 +0300

    new ps script

---
+++ CreatPipline/storang-and-vm.ps1
@@ -0,0 +1,27 @@
+$rg = 'sotarge-accounts-assignment'
+$storageName = Read-Host -Prompt "Enter the Storage name"
+
+New-AzResourceGroup -Name $rg -Location northeurope -Force
+
+New-AzResourceGroupDeployment `
+    -Name 'newstoragemendy' `
+    -ResourceGroupName $rg `
+    -TemplateFile 'storage-accounts.json' `
+    -storageName $storageName
+
+
+#$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+#$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+#$adminUsername = Read-Host -Prompt "Enter the administrator username"
+#$adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
+#$dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP"
+
+#New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+#New-AzResourceGroupDeployment `
+    #-ResourceGroupName $resourceGroupName `
+    #-TemplateFile "vm-windwos-server.json" `
+    #-adminUsername $adminUsername `
+    #-adminPassword $adminPassword `
+    #-dnsLabelPrefix $dnsLabelPrefix
+
+ #(Get-AzVm -ResourceGroupName $resourceGroupName).name
