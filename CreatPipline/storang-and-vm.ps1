f49df73d8373d54d7dc4c283bb5b163bda255802
Mendy Ben Ezra <73079004+mendi2770@users.noreply.github.com>
2022-08-08 16:59:32 +0300

    Update storang-and-vm.ps1

--- CreatPipline/storang-and-vm.ps1
+++ CreatPipline/storang-and-vm.ps1
@@ -7,7 +7,6 @@ New-AzResourceGroupDeployment `
     -Name 'newstoragemendy' `
     -ResourceGroupName $rg `
     -TemplateUri 'https://raw.githubusercontent.com/mendi2770/AzurAssignment/main/CreatPipline/storage-accounts.json' `
-    -Force
 
 
 #$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
